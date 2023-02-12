4b13c5304462a470adc5123cdc2323c034483794
teofilac <teofilac@student.21-school.ru>
2023-02-12 16:51:22 +0300

    polish_2

--- src/polish_notation.c
+++ src/polish_notation.c
@@ -21,8 +21,8 @@ int is_empty(struct Stack *stack) {
   return stack->top == -1;
 }
 
-double evaluate(char* expression) {
-  struct Stack stack = {.top = -1};
+double calculate(char* expression) {
+  struct stack top = NULL;
   char *token = strtok(expression, " ");
   while (token != NULL) {
     if (strcmp(token, "+") == 0) {
@@ -41,10 +41,24 @@ double evaluate(char* expression) {
       double operand2 = pop(&stack);
       double operand1 = pop(&stack);
       push(&stack, operand1 / operand2);
-    } else if (strcmp(token, "^") == 0) {
-      double operand2 = pop(&stack);
+    } else if (strcmp(token, "sin") == 0) {
       double operand1 = pop(&stack);
-      push(&stack, pow(operand1, operand2));
+      push(&stack, sin(operand1));
+    } else if (strcmp(token, "cos") == 0) {
+      double operand1 = pop(&stack);
+      push(&stack, cos(operand1));
+    } else if (strcmp(token, "tan") == 0) {
+      double operand1 = pop(&stack);
+      push(&stack, tan(operand1));
+    } else if (strcmp(token, "ctg") == 0) {
+      double operand1 = pop(&stack);
+      push(&stack, ctg(operand1));
+    } else if (strcmp(token, "sqrt") == 0) {
+      double operand1 = pop(&stack);
+      push(&stack, sqrt(operand1));
+    } else if (strcmp(token, "ln") == 0) {
+      double operand1 = pop(&stack);
+      push(&stack, ln(operand1));
     } else {
       push(&stack, atof(token));
     }
@@ -52,4 +66,3 @@ double evaluate(char* expression) {
   }
   return pop(&stack);
 }
-
